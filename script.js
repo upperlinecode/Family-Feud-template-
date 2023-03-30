9d042c457b6580ccb2e93381f965a3a0d4913f60
Ileini Melendez <122940019+ileini-melendez@users.noreply.github.com>
2023-03-30 14:27:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//check to ensure the script.js file is linked
+console.log("hey, the script is working!")
+//Step 1: Grab our elements in JS using querySelector
+
+// Step 2: Add event listeners
+
+// Step 3: Write a function 
+function checkAnswer(){
+  
+}
+
+function eraseInput(){
+
+}
+function eraseAnswers(){
+  
+}
